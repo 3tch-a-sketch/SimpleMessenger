679dfea226a991a0ecd8f291d4014b1e641f89c2
3tch-a-sketch <sametchells@icloud.com>
2022-05-07 17:03:15 +0100

    created project files

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='grid place-items-center h-screen'>
+      <div className='bg-gray-700 text-zinc-50 p-4'>
+        test
+      </div>
+    </div>
+  );
+}
+
+export default App;
